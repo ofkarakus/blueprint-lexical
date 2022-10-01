13c14a69302dc35c2a40f66009c0f6bb8b971dff
Omer Karakus <omer.karakus@lonres.com>
2022-10-01 06:38:13 +0100

    remove medium-editor

--- src/components/molecules/Template/index.tsx
+++ src/components/molecules/Template/index.tsx
@@ -1,17 +1,6 @@
-import MediumEditor from "medium-editor";
-import { useEffect } from "react";
 import "./styles.css";
 
 const Template = () => {
-  useEffect(() => {
-    const editor = new MediumEditor(".editable", {
-      placeholder: {
-        text: "",
-      },
-    });
-    editor.setContent("lonres.com");
-  }, []);
-
   return <textarea name="editor" className="editable"></textarea>;
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-require("medium-editor/dist/css/medium-editor.css");
-require("medium-editor/dist/css/themes/default.css");
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
